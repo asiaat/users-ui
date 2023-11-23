4f97f2e4b654c056d6a8b30e0ce5622b684d87f6
kalleolumets <olumetskalle@gmail.com>
2023-11-23 22:46:19 +0200

    just console.log for network debugging

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,19 @@
+<script setup>
+import { ref, onMounted } from "vue";
+import axios from "axios";
 
+const user = ref();
+
+onMounted(async () => {
+    const data = await axios.get('/api/user');
+    console.log(data);
+});
+</script>
 <template>
   <div class="max-w-7xl mx-auto">
     <h1>Home</h1>
+    <h1>
+        {{ user?.name }}
+    </h1>
   </div>
 </template>
